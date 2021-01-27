af222c820d5a7b9497bff7db861fe494e9272875
student <student@ip-172-31-60-47.us-west-2.compute.internal>
2021-01-27 15:28:19 -0800

    simon says

--- src/_01_nasty_surprise/NastySurprise.java
+++ src/_01_nasty_surprise/NastySurprise.java
@@ -2,15 +2,19 @@ package _01_nasty_surprise;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class NastySurprise implements ActionListener {
-	JButton trick = new JButton();
-	JButton treat = new JButton();
+	JButton trick = new JButton("trick");
+	JButton treat = new JButton("treat");
 	NastySurprise(){
 JPanel panel = new JPanel();
 		
@@ -33,7 +37,10 @@ JPanel panel = new JPanel();
 		panel.add(treat);
 		frame.pack();
 		
-		trick.getActionListeners();
+		//trick.addActionListener(this);
+		//treat.getActionListeners();
+		
+		
 		
 		
 			
@@ -50,8 +57,29 @@ JPanel panel = new JPanel();
 		// TODO Auto-generated method stub
 		
 		if(arg0.getSource()==trick) {
-			
+		//cat.jpg
+		showPictureFromTheInternet("https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.rd.com%2Fwp-content%2Fuploads%2F2019%2F05%2FAmerican-shorthair-cat.jpg&imgrefurl=https%3A%2F%2Fwww.rd.com%2Flist%2Fcutest-cat-breeds%2F&tbnid=S_fAjEZ3GR3VEM&vet=12ahUKEwiDqrCgl73uAhUQL6wKHVerBSIQMygAegUIARDGAQ..i&docid=O2KxBoLWpCWtlM&w=4734&h=3555&q=cute%20cat&safe=active&ved=2ahUKEwiDqrCgl73uAhUQL6wKHVerBSIQMygAegUIARDGAQ");
+		//showPictureFromTheInternet("Cat.jpg");
 		}
+		if(arg0.getSource()==treat) {
+			//scary.jpg
+			showPictureFromTheInternet("https://www.google.com/imgres?imgurl=https%3A%2F%2Fi1.sndcdn.com%2Fartworks-000492612906-hcoicz-t500x500.jpg&imgrefurl=https%3A%2F%2Fsoundcloud.com%2Fromeoblue69%2Fmoto-moto-big-and-chunky&tbnid=KJXj0Fs5oAgxdM&vet=12ahUKEwi81ovVl73uAhVCfqwKHexzBbYQMygNegUIARDTAQ..i&docid=yYq6Ye223XRCKM&w=500&h=500&q=moto%20moto&safe=active&ved=2ahUKEwi81ovVl73uAhVCfqwKHexzBbYQMygNegUIARDTAQ");
+	//showPictureFromTheInternet("scary.jpg");
+		}
+	}
+	
+	private void showPictureFromTheInternet(String imageUrl) {
+	    try {
+	        URL url = new URL(imageUrl);
+	        Icon icon = new ImageIcon(url);
+	        JLabel imageLabel = new JLabel(icon);
+	        JFrame frame = new JFrame();
+	        frame.add(imageLabel);
+	        frame.setVisible(true);
+	        frame.pack();
+	    } catch (MalformedURLException e) {
+	        e.printStackTrace();
+	    }
 	}
 
 	
